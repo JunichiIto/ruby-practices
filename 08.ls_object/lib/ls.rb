f6496e1edf6f8b7416e926d5be624332662658ff
Junichi Ito <jit@sonicgarden.jp>
2022-04-28 13:32:26 +0900

    Implement short format

--- 08.ls_object/lib/ls.rb
+++ 08.ls_object/lib/ls.rb
@@ -1,17 +1,16 @@
 require_relative 'params'
 require_relative 'ls_file'
+require_relative 'views/short_format'
 
 class Ls
   def main
     params = Params.new(ARGV)
-    @ls_files = LsFile.all(params)
-    puts @ls_files
-    # ファイルの一覧をviewに渡す
-    # if params.long_format?
-    #   render 'long'
-    # else
-    #   render 'short'
-    # end
+    ls_files = LsFile.all(params)
+    if params.long_format?
+      render 'long'
+    else
+      puts Views::ShortFormat.new(ls_files).format
+    end
   end
 end
 
