4dc394ccd728742bfc636cd1e8c23a3659ec09be
Junichi Ito <jit@sonicgarden.jp>
2022-04-28 14:05:33 +0900

    Implement long format

--- 08.ls_object/lib/ls.rb
+++ 08.ls_object/lib/ls.rb
@@ -1,13 +1,14 @@
 require_relative 'params'
 require_relative 'ls_file'
 require_relative 'views/short_format'
+require_relative 'views/long_format'
 
 class Ls
   def main
     params = Params.new(ARGV)
     ls_files = LsFile.all(params)
     if params.long_format?
-      render 'long'
+      puts Views::LongFormat.new(ls_files).format
     else
       puts Views::ShortFormat.new(ls_files).format
     end
---
+++ 08.ls_object/lib/views/long_format.rb
@@ -0,0 +1,64 @@
+module Views
+  class LongFormat
+    FILE_TYPE_TABLE = {
+      'file' => '-',
+      'directory' => 'd',
+    }.freeze
+
+    PERMISSION_TABLE = {
+      '1' => '--x',
+      '2' => '-w-',
+      '3' => '-wx',
+      '4' => 'r--',
+      '5' => 'r-x',
+      '6' => 'rw-',
+      '7' => 'rwx',
+    }.freeze
+
+    def initialize(ls_files)
+      @ls_files = ls_files
+    end
+
+    def format
+      total = @ls_files.sum(&:block_size)
+      header = "total #{total}"
+      max_size_table = generate_max_size_table
+      body = @ls_files.map do |ls_file|
+        columns = []
+        columns << format_mode(ls_file) + ' '
+        columns << ls_file.link_count.to_s.rjust(max_size_table[:link_count])
+        columns << ls_file.owner_name.ljust(max_size_table[:owner_name]) + ' '
+        columns << ls_file.group_name.ljust(max_size_table[:group_name]) + ' '
+        columns << ls_file.bytesize.to_s.rjust(max_size_table[:bytesize])
+        columns << ls_file.mtime.strftime("%b %d %H:%M")
+        columns << ls_file.name
+        columns.join(' ')
+      end
+      [header, *body].join("\n")
+    end
+
+    private
+
+    def format_mode(ls_file)
+      file_type = FILE_TYPE_TABLE[ls_file.file_type]
+      permission = ls_file.permission.each_char.map { |c| PERMISSION_TABLE[c] }.join
+      file_type + permission
+    end
+
+    def generate_max_size_table
+      ret = {
+        link_count: 0,
+        owner_name: 0,
+        group_name: 0,
+        bytesize: 0,
+      }
+      @ls_files.each do |ls_file|
+        ret[:link_count] = [ret[:link_count], ls_file.link_count.to_s.size].max
+        ret[:owner_name] = [ret[:owner_name], ls_file.owner_name.size].max
+        ret[:group_name] = [ret[:group_name], ls_file.group_name.size].max
+        ret[:bytesize] = [ret[:bytesize], ls_file.bytesize.to_s.size].max
+      end
+      ret
+    end
+  end
+end
