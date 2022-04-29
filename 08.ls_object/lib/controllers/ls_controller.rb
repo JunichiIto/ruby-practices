979d6ff0db14b9aba2d323d228a7715ff0c3c064
Junichi Ito <jit@sonicgarden.jp>
2022-04-29 11:31:47 +0900

    Move params to method argument

--- 08.ls_object/lib/controllers/ls_controller.rb
+++ 08.ls_object/lib/controllers/ls_controller.rb
@@ -6,8 +6,7 @@ require_relative '../views/short_format'
 require_relative '../views/long_format'
 
 class LsController
-  def main
-    params = Params.new(ARGV)
+  def main(params = Params.new(ARGV))
     ls_files = LsFile.all(params)
     if params.long_format?
       puts LongFormat.new(ls_files).render
