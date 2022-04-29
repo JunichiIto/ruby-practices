4b7a68b9d7368ca914722299d184ef06a95f3d1a
Junichi Ito <jit@sonicgarden.jp>
2022-04-29 12:32:50 +0900

    Organize test

--- 08.ls_object/lib/ls_app.rb
+++ 08.ls_object/lib/ls_app.rb
@@ -6,7 +6,8 @@ require_relative 'views/short_format'
 require_relative 'views/long_format'
 
 class LsApp
-  def main(params = Params.new(ARGV))
+  def main(argv = ARGV)
+    params = Params.new(argv)
     ls_files = LsFile.all(params)
     if params.long_format?
       puts LongFormat.new(ls_files).render
