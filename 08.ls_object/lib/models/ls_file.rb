6d48616a3892c6bc13abdfba3b344cc6a60d20bf
Junichi Ito <jit@sonicgarden.jp>
2022-04-30 08:24:57 +0900

    Pass integer instead of array

--- 08.ls_object/lib/models/ls_file.rb
+++ 08.ls_object/lib/models/ls_file.rb
@@ -6,8 +6,8 @@ require 'pathname'
 class LsFile
   def self.all(params)
     pattern = File.join(params.target_directory, '*')
-    flags = params.dot_match? ? [File::FNM_DOTMATCH] : []
-    paths = Dir.glob(pattern, *flags)
+    flag = params.dot_match? ? File::FNM_DOTMATCH : 0
+    paths = Dir.glob(pattern, flag)
     paths << File.join(params.target_directory, '..') if params.dot_match?
     sorted_paths = params.reverse? ? paths.sort.reverse : paths.sort
     sorted_paths.map { |path| LsFile.new(path) }
